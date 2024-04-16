f654e4d4edd4a351ed3871afd70dab0eaccef338
Muideen7 <Olayeyeayomide2000@gmail.com>
2024-04-16 20:13:38 +0100

    variable.js

---
+++ 01_Day_Introduction/variable.js
@@ -0,0 +1,18 @@
+// Declaring different variables of different data types
+let firstName = "Ayomide"; // first name of a person
+let lastName = "Olayeye"; // last name of a personq
+let country = "Nigeria"; // country
+let city = "Lagos"; // capital city
+let age = 24; // age in years
+let isMarried = false; // marital status
+
+// Declaring variables with number values
+
+const gravity = 9.81; // earth gravity  in m/s2
+const boilingPoint = 100; // water boiling point, temperature in oC
+const PI = 3.14; // geometrical constant
+
+// Variables can also be declaring in one line separated by comma
+let name = "Ayomide", //name of a person
+  job = "student",
+  live = "Nigeria";
